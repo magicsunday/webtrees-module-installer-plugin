16f69949045a9400a310504aade7f2a5fc3e67b4
Rico Sonntag <mail@ricosonntag.de>
2025-08-14 07:27:33 +0200

    Rework module installation process

--- src/Composer/ModuleInstaller.php
+++ src/Composer/ModuleInstaller.php
@@ -11,125 +11,129 @@ declare(strict_types=1);
 
 namespace MagicSunday\Webtrees\Composer;
 
-use Composer\Composer;
+use Composer\Config;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
-use MagicSunday\Webtrees\Composer\Plugin\Config;
 use React\Promise\PromiseInterface;
 
+use ReflectionClass;
+use function is_string;
+use function React\Promise\resolve;
+
 /**
- * Handles the installation of webtrees modules by managing the installation path,
- * verifying package types, and configuring plugin settings.
- *
- * @author  Rico Sonntag <mail@ricosonntag.de>
- * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License v3.0
- * @link    https://github.com/magicsunday/webtrees-module-installer-plugin/
+ * ModuleInstaller is responsible for handling the installation of packages with the type `webtrees-module`.
+ * It integrates with Composer's library installation process and ensures modules are correctly placed
+ * into the appropriate path within the `fisharebest/webtrees` directory structure if installed.
  */
 class ModuleInstaller extends LibraryInstaller
 {
-    /**
-     * The supported package type.
-     */
+    public const ROOT_PACKAGE_NAME = 'fisharebest/webtrees';
+
     public const PACKAGE_TYPE = 'webtrees-module';
 
-    /**
-     * The directory used to install the module into.
-     */
-    private const MODULES_DIR = 'modules_v4' . DIRECTORY_SEPARATOR;
+    private bool $skipInstall = false;
 
-    /**
-     * @var Config
-     */
-    private Config $pluginConfig;
-
-    /**
-     * Checks that provided package is installed.
-     *
-     * @param InstalledRepositoryInterface $repo
-     * @param PackageInterface             $package
-     *
-     * @return bool
-     */
-    public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package): bool
-    {
-        // Always return "false" to reinstall the packages after installing/updating Webtrees.
-        // For example, a downgrade may update only the "fisharebest/webtrees" package, while
-        // the "webtrees-module" packages remain. However, since these packages must be installed
-        // within the "fisharebest/webtrees" package, the "modules_v4" directory will otherwise be empty.
-        return false;
-    }
-
-    /**
-     * Retrieves the installation path for the given package.
-     *
-     * @param PackageInterface $package the package for which the installation path is determined
-     *
-     * @return string the computed installation path for the specified package
-     */
     public function getInstallPath(PackageInterface $package): string
     {
-        $separatorPos = strpos($package->getPrettyName(), DIRECTORY_SEPARATOR);
-        $modulePath   = self::MODULES_DIR;
+        $parts           = explode(DIRECTORY_SEPARATOR, $package->getPrettyName());
+        $packageBaseName = end($parts);
+        $webtreesPath    = $this->findWebtreesBasePath();
 
-        if ($separatorPos !== false) {
-            $modulePath .= substr($package->getPrettyName(), $separatorPos + 1);
+        if ($webtreesPath === null) {
+            $vendorDirectory = $this->composer->getConfig()->get('vendor-dir');
+
+            if (is_string($vendorDirectory)) {
+                $webtreesPath = $vendorDirectory
+                    . DIRECTORY_SEPARATOR . 'fisharebest'
+                    . DIRECTORY_SEPARATOR . 'webtrees';
+            }
         }
 
-        return $this->getAppDirectory() . DIRECTORY_SEPARATOR . $modulePath;
+        return $webtreesPath
+            . DIRECTORY_SEPARATOR . 'modules_v4'
+            . DIRECTORY_SEPARATOR . $packageBaseName;
     }
 
-    /**
-     * Installation step - store the path for later processing.
-     *
-     * @param InstalledRepositoryInterface $repo    The repository in which to check
-     * @param PackageInterface             $package The package instance
-     *
-     * @return PromiseInterface<void|null>|null The installation path or a promise
-     */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package): ?PromiseInterface
     {
-        $promise = parent::install($repo, $package);
+        // Skipping installation of the package, it's triggered by the
+        // POST_PACKAGE_INSTALL and POST_PACKAGE_UPDATE events instead
+        if ($this->skipInstall) {
+            return resolve(null);
+        }
 
-        /** @var Composer $composer */
-        $composer = $this->composer;
+        return parent::install($repo, $package);
+    }
 
-        return $promise?->then(
-            function () use ($composer, $package): void {
-                $installPath = $this->getInstallPath($package);
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target): ?PromiseInterface
+    {
+        if ($this->skipInstall) {
+            return resolve(null);
+        }
 
-                // Get the plugin instance to store the pending module
-                $plugins = $composer->getPluginManager()->getPlugins();
+        return parent::update($repo, $initial, $target);
+    }
 
-                foreach ($plugins as $plugin) {
-                    if ($plugin instanceof ModuleInstallerPlugin) {
-                        $plugin->addPendingModule($package, $installPath);
-                        break;
-                    }
-                }
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package): ?PromiseInterface
+    {
+        if ($this->skipInstall) {
+            return resolve(null);
+        }
+
+        return parent::uninstall($repo, $package);
+    }
+
+    public function findWebtreesBasePath(): ?string
+    {
+        $webtreesPackage = $this->composer
+            ->getRepositoryManager()
+            ->getLocalRepository()
+            ->findPackage(
+                self::ROOT_PACKAGE_NAME,
+                '*'
+            );
+
+        // Return installation path of webtrees package
+        if ($webtreesPackage !== null) {
+            $path = $this->composer
+                ->getInstallationManager()
+                ->getInstallPath($webtreesPackage);
+
+            if (is_string($path) && is_dir($path)) {
+                return rtrim($path, DIRECTORY_SEPARATOR);
             }
-        );
+        }
+
+        // If webtrees exists as root package, return the base directory
+        if ($this->composer->getPackage()->getName() === self::ROOT_PACKAGE_NAME) {
+            return $this->getComposerBaseDir($this->composer->getConfig());
+        }
+
+        return null;
     }
 
     /**
-     * Retrieves the application directory path based on the plugin configuration.
+     * @param Config $config
      *
-     * @return string the application directory path or an empty string if not configured
+     * @return string|null
      */
-    private function getAppDirectory(): string
+    private function getComposerBaseDir(Config $config): ?string
     {
-        return $this->pluginConfig->get('app-dir') ?? '';
+        $reflectionClass    = new ReflectionClass($config);
+        $reflectionProperty = $reflectionClass->getProperty('baseDir');
+
+        $value = $reflectionProperty->getValue($config);
+
+        if (is_string($value)) {
+            return $value;
+        }
+
+        return null;
     }
 
-    /**
-     * Sets the plugin configuration.
-     *
-     * @param Config $pluginConfig the configuration object for the plugin
-     *
-     * @return void
-     */
-    public function setPluginConfig(Config $pluginConfig): void
+    public function skipInstallation(bool $skip): void
     {
-        $this->pluginConfig = $pluginConfig;
+        $this->skipInstall = $skip;
     }
 }
